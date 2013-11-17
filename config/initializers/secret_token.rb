565d53d072a63f21150d45a6e4f03bb3513f3d8e
Buddy Magsipoc <keikun17@gmail.com>
2013-11-17 19:24:47 +0800

    Force user to set key on prod。

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,10 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Hots::Application.config.secret_key_base = 'af8c46658708790086f9b6d6271d786e02270fb279b5ee7879d2207494ccdbac967c1736b78fa32e6e30b134ad0561bb4180fc5167251bcac8d2495eb9c23c12'
+
+if !Rails.env.production?
+  Hots::Application.config.secret_key_base = 'af8c46658708790086f9b6d6271d786e02270fb279b5ee7879d2207494ccdbac967c1736b78fa32e6e30b134ad0561bb4180fc5167251bcac8d2495eb9c23c12'
+else
+  raise "You must set a secret token in ENV['SECRET_TOKEN'] or in config/initializers/secret_token.rb" if ENV['SECRET_TOKEN'].blank?
+  Hots::Application.config.secret_key_base = ENV['SECRET_TOKEN']
+end
