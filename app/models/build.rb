11bea1a794d9f0c34fc6c3b8ccbe42e1a913de6d
Buddy Magsipoc <keikun17@gmail.com>
2013-11-29 00:35:28 +0800

    Annotate。

--- app/models/build.rb
+++ app/models/build.rb
@@ -2,3 +2,17 @@ class Build < ActiveRecord::Base
   belongs_to :hero
   validates :hero_id, presence: true
 end
+
+# == Schema Information
+#
+# Table name: builds
+#
+#  id         :integer          not null, primary key
+#  title      :string(255)
+#  summary    :text
+#  body       :text
+#  hero_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
